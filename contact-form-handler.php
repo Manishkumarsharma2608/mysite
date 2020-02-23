6d802bcaca51b169718a205a89921e4f7ffdfac0
Manish Kumar Sharma <60810480+Manishkumarsharma2608@users.noreply.github.com>
2020-02-23 13:21:06 +0530

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,24 @@
+<? php
+        $name =$_POST['name'];
+        $visitor_email=$_POST['email'];
+        $phone=$_POST['phone'];
+        $message=$_POST['message'];
+
+
+        $email_from = $visitor_email;
+
+        $email_subject = 'New Form Submission';
+
+        $email_body = "User name: $name.\n";
+                        "User email: $visitor_email.\n";
+                            "User phone: $phone.\n";
+                              "User mesage: $message.\n";
+
+        $to ="mkssharma08@gmail.com";
+        
+        $headers = "From: $email_from \r\n";
+        $headers = "Reply-To: $visitor_email \r\n";
+        mail($to,$email_subject,$email_body,$headers);
+        header("Location: contact.html");
+
+?>
